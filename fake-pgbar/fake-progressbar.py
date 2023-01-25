7bc1d0085d58f7eee607741fde80ac60c759fb23
gus <gustavo1sad@gmail.com>
2023-01-25 02:10:25 -0300

    Lib config

---
+++ fake-pgbar/fake-progressbar.py
@@ -0,0 +1,16 @@
+import os
+
+def progressBar(current, total):
+    
+    percent = current/(total/100)
+    print("["+"█"*int(percent)+"░"*(100-int(percent))+f"] {round(percent,2)}% {current}/{total}")
+    
+    if(current == total):
+        input()
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+for i in range(3983):
+    progressBar(i, 3982)
+
